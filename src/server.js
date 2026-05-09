6e2b97a114f8d51456102bc637d0254fe782e4e4
afifrohul <afifmemyself22@gmail.com>
2026-05-09 19:28:28 +0700

    initial

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+const host = 'localhost';
+
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+
+app.listen(port, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
