944831325a12d16fe7ad85d78cf56e4dbb1a208c
afifrohul <afifmemyself22@gmail.com>
2026-05-11 09:22:55 +0700

    feat: add predict endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes.js';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(cors({
   origin: '*'
 }));
+
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
